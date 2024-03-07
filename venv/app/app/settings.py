f299607d1e7d1b73682b769dfbe18899c2caf879
lllMaKsImlll <148184294+lllMaKsImlll@users.noreply.github.com>
2024-03-07 09:19:32 +0500

    version v1

--- venv/app/app/settings.py
+++ venv/app/app/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'exchange_app'
+    'exchange_app',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
